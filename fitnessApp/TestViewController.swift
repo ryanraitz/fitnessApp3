60ece29fe3088734bb6a192de9ed3d92bddbaf75
Ryan Raitz <ryanraitz96@gmail.com>
2022-01-03 03:00:58 -0500

    change

--- fitnessApp/TestViewController.swift
+++ fitnessApp/TestViewController.swift
@@ -53,20 +53,9 @@ class TestViewController : UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad()
     {
-       /* super.viewDidLoad()
-        
-        //Navigation bar styling
-        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        self.navigationController?.navigationBar.shadowImage = UIImage()
-        self.navigationController?.navigationBar.isTranslucent = true
-        self.navigationController?.view.backgroundColor = .clear
-        
-        //Adding shadow to title label
-        myLabel.layer.shadowColor = UIColor.black.cgColor
-        myLabel.layer.shadowOffset = CGSize(width: 10, height: 10)
-        myLabel.layer.shadowRadius = 10
-        myLabel.layer.shadowOpacity = 1.0
-        */
+       
+        if selectedLocation?.videoURL != "pyramid"
+        {
         let url = selectedLocation?.videoURL
         guard let videoLink = URL(string: url!) else { return }
         //print(url!)
@@ -81,12 +70,48 @@ class TestViewController : UIViewController, WKNavigationDelegate {
         //UIApplication.shared.open(videoLink)
         //UIApplication.shared.open(videoLink)
         webView.load(URLRequest(url: videoLink))
+        }
+        else
+        {
+            super.viewDidLoad()
+            
+            //Navigation bar styling
+            self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+            self.navigationController?.navigationBar.shadowImage = UIImage()
+            self.navigationController?.navigationBar.isTranslucent = true
+            self.navigationController?.view.backgroundColor = .clear
+            
+            //Adding shadow to title label
+            myLabel.layer.shadowColor = UIColor.black.cgColor
+            myLabel.layer.shadowOffset = CGSize(width: 10, height: 10)
+            myLabel.layer.shadowRadius = 10
+            myLabel.layer.shadowOpacity = 1.0
+        }
         
     }
     
-    /*override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear()
-         Initialize cell text labels to desired info
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if selectedLocation?.videoURL != "pyramid"
+        {
+        let url = selectedLocation?.videoURL
+        guard let videoLink = URL(string: url!) else { return }
+        //print(url!)
+        //let player = AVPlayer(url: videoLink)
+        //let playerViewController = AVPlayerViewController()
+        //playerViewController.player = player
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+        //self.present(playerViewController, animated: true) {
+        //playerViewController.player?.play()
+        //UIApplication.shared.open(videoLink)
+        //UIApplication.shared.open(videoLink)
+        webView.load(URLRequest(url: videoLink))
+        }
+        else
+        {
+         // Initialize cell text labels to desired info
         myTextLabel.text = selectedLocation?.exerciseDescription
         
         //Dynamically size overview text field based on content
@@ -96,27 +121,8 @@ class TestViewController : UIViewController, WKNavigationDelegate {
         //Dynamically size mind muscle connection text field based on content
         mindMuscleConnectionTextLabel.text = selectedLocation?.mmC
         mindMuscleConnectionTextLabel.sizeToFit()
+        }
         
     }
-    */
-    /*  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-     
-     let touch = touches.first!
-     //Make sure video only plays if user clicks in the video box
-     if touch.view == myImage
-     {
-     print("touched")
-     guard let path = Bundle.main.path(forResource: selectedLocation?.videoURL,
-     ofType: "mp4") else { return }
-     let videoLink = URL(fileURLWithPath: path)
-     let player = AVPlayer(url: videoLink)
-     let playerViewController = AVPlayerViewController()
-     playerViewController.player = player
-     
-     self.present(playerViewController, animated: true) {
-     playerViewController.player?.play()
-     }
-     }
-     }
-     */
+    
 }
